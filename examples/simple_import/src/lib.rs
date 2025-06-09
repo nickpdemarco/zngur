ee0e06b5ab8a049929fbe3245de82169b9ac16ad
Nick DeMarco <demarco@adobe.com>
2025-06-09 11:22:44 -0400

    Remove needless AppUtils in example

--- examples/simple_import/src/lib.rs
+++ examples/simple_import/src/lib.rs
@@ -2,13 +2,3 @@
 
 #[rustfmt::skip]
 mod generated;
-
-/// Application utilities for data processing
-pub struct AppUtils;
-
-impl AppUtils {
-    /// Process a vector of numbers and return a summary value (sum of even numbers)
-    pub fn process_numbers(numbers: std::vec::Vec<i32>) -> i32 {
-        numbers.into_iter().filter(|&x| x % 2 == 0).sum()
-    }
-}
